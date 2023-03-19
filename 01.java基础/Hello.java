71f2e629a7f91601f3c66c15d4b380bcabd35bb5
llcoolmxdx5 <2938129812@qq.com>
2023-03-19 21:28:54 +0800

    feat: 添加路线图,快速开始

---
+++ 01.java基础/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Hello
+ */
+// Hello 是一个公有类 和文件名相同
+public class Hello {
+    // main 是函数运行的默认入口
+    public static void main(String[] args) {
+        System.out.println(4/3.23);
+    }
+}
